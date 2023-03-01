7a7c553a4cda9354ea9fb836b36506d5b21d8cd6
nicola00 <nicolajobs00@gmail.com>
2023-03-01 18:12:34 +0100

    FEAT add after on footer section

--- src/components/comics-header.vue
+++ src/components/comics-header.vue
@@ -11,31 +11,38 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div class="header">
-            <div>
-                <a href="#">
-                    <img class="logo" src="vue-dc-comics-1/img/favicon.ico" alt="">
-                </a>
+    <div class="header">
+        <div class="container">
+            <div class="head-section">
+                <div>
+
+                    <a href="#">
+                        <img class="logo" src="vue-dc-comics-1/img/favicon.ico" alt="">
+                    </a>
+                </div>
+                <ul class="nav">
+                    <li class="item-nav active">Lorem</li>
+                    <li class="item-nav">Lorem</li>
+                    <li class="item-nav">Lorem</li>
+                    <li class="item-nav">Lorem</li>
+                    <li class="item-nav">Lorem</li>
+                    <li class="item-nav">Lorem</li>
+                    <li class="item-nav">Lorem</li>
+                    <li class="item-nav">Lorem</li>
+                    <li class="item-nav">Lorem</li>
+                    <li class="item-nav">Lorem</li>
+                </ul>
             </div>
-            <ul class="nav">
-                <li class="item-nav active">Lorem</li>
-                <li class="item-nav">Lorem</li>
-                <li class="item-nav">Lorem</li>
-                <li class="item-nav">Lorem</li>
-                <li class="item-nav">Lorem</li>
-                <li class="item-nav">Lorem</li>
-                <li class="item-nav">Lorem</li>
-                <li class="item-nav">Lorem</li>
-                <li class="item-nav">Lorem</li>
-                <li class="item-nav">Lorem</li>
-            </ul>
         </div>
     </div>
 </template>
 
 <style>
 .header {
+    position: relative;
+}
+
+.head-section {
     display: flex;
     justify-content: space-between;
     padding: 20px 0;
@@ -68,14 +75,38 @@ export default {
 }
 
 .item-nav:hover {
+    color: rgb(15, 124, 197);
+}
+
+.item-nav:hover::after {
     opacity: 0.9;
     color: rgb(15, 124, 197);
+    content: '';
+    display: block;
+    width: 40px;
+    height: 5px;
+    background-color: rgb(15, 124, 197);
+    margin: auto;
+    position: absolute;
+    bottom: 0;
+
 }
 
 .item-nav.active {
     color: rgb(15, 124, 197);
 }
 
+.active::after {
+    content: '';
+    display: block;
+    width: 40px;
+    height: 5px;
+    color: rgb(15, 124, 197);
+    background-color: rgb(15, 124, 197);
+    margin: auto;
+    position: absolute;
+    bottom: 0;
+}
 
 ul {
     list-style-type: none;
