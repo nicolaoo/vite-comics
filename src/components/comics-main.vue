eb8c8df9e390f5fa1b23cadf8c2fea5545bb178d
nicola00 <nicolajobs00@gmail.com>
2023-03-02 16:42:26 +0100

    FEAT ciclo for-in

--- src/components/comics-main.vue
+++ src/components/comics-main.vue
@@ -4,10 +4,76 @@ export default {
         return {
             heroCard: [
                 {
-                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                    "thumb": "https://picsum.photos/200/300",
                     "price": "$19.99",
                     "series": "Action Comics",
                     "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://picsum.photos/200/300",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
                 }
             ],
         }
@@ -23,103 +89,14 @@ export default {
         <div class="comics-bg-black">
             <div class="container">
                 <div class="grid">
-                    <div class="card">
+                    <div v-for="(el, i) in heroCard" :key="i" class="card">
                         <figure>
                             <img src="https://picsum.photos/200/300" alt="">
                         </figure>
                         <p>
-                            Action Comics
+                            {{ el.series }}
                         </p>
                     </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://picsum.photos/200/300" alt="">
-                        </figure>
-                        <p>
-                            Action Comics
-                        </p>
-                    </div>
-
                 </div>
             </div>
         </div>
@@ -180,7 +157,7 @@ export default {
 .grid {
     display: grid;
     grid-template-columns: repeat(6, 1fr);
-    column-gap: 10px;
+    gap: 20px;
     padding: 20px 0;
 }
 
